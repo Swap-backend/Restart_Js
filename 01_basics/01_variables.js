65b2258c95a65ee9ad20f79e564eda16c7952ad0
Swap-backend <abymemp111@gmail.com>
2025-09-15 13:03:41 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const acountid = 123456 //   it means constant variable value can not be changed 
+let accountemail = "swapnil56@gmail.com"
+var password = "password@1234"
+accountcity = "udipur"
+
+// acountid = 2 - not allowed 
+accountemail="rahul@gmail.com"
+password="swap@1234"
+accountcity="raipur"
+
+
+// another method of console 2-3 fileds toghter :-
+
+console.table([accountemail,password,accountcity]);
+
+console.log(acountid);
